95ec21e0bfa608935f9f42b3a8f2c619d9fde287
Birgitt Thomsen <birgitt.thomsen@t-online.de>
2026-07-05 22:42:38 +0200

    Add movie service to handle fetching movie data from the omdb api

---
+++ movie_service.py
@@ -0,0 +1,37 @@
+"""This script handles interaction with the omdb api to fetch movie data
+before passing it on to the Flask routes."""
+
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+REQUEST_URL = "https://www.omdbapi.com/"
+
+def get_movie_from_omdb(title):
+	try:
+		params = {'apikey': API_KEY, 't': title}
+		response = requests.get(REQUEST_URL,
+		                        params=params,
+		                        timeout=5)
+
+		response.raise_for_status()
+
+		data = response.json()
+
+		if data.get("Response") == "False":
+			return None
+
+		return {
+			"name": data["Title"],
+			"director": data["Director"],
+			"year": int(data["Year"]) if data["Year"].isdigit() else None,
+			"imdb_id": data["imdbID"],
+			"poster_url": None if data["Poster"] == "N/A" else data["Poster"],
+		}
+
+	except requests.exceptions.RequestException:
+		return None
+
+print(get_movie_from_omdb('Moneyball'))
