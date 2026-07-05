5da1d3c62afc97d22b75424a91f6975f8d31660b
Birgitt Thomsen <birgitt.thomsen@t-online.de>
2026-07-05 21:38:08 +0200

    Define basic CRUD methods in DataManager class to interact with the database

---
+++ data_manager.py
@@ -0,0 +1,47 @@
+""" This script handles the interaction with the movie database """
+from models import db, User, Movie
+
+class DataManager():
+
+    def create_user(self, name):
+        new_user = User(name=name)
+        db.session.add(new_user)
+        db.session.commit()
+
+
+    def get_users(self):
+        return User.query.all()
+
+
+    def get_movies(self, user_id):
+        return Movie.query.filter_by(user_id=user_id).all()
+
+
+    def add_movie(self, movie):
+        db.session.add(movie)
+        db.session.commit()
+        return movie
+
+
+    def update_movie(self, movie_id, new_title):
+        movie = db.session.get(Movie, movie_id)
+
+        if movie is None:
+            return None
+
+        movie.name = new_title
+        db.session.commit()
+
+        return movie
+
+
+    def delete_movie(self, movie_id):
+        movie = db.session.get(Movie, movie_id)
+
+        if movie is None:
+            return False
+
+        db.session.delete(movie)
+        db.session.commit()
+
+        return True
